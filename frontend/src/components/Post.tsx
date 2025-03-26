8346baf7cd872515ca0d66b55d98a653c8270544
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-03-27 01:52:21 +0300

    fixed the post fetching

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,61 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Post = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/posts")
+      .then((res) => {
+        setPosts(res.data);
+        console.log("Posts fetched successfully:", res.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching posts:", err);
+      });
+  }, []);
+
   return (
-    <>
-      <section className=" mt-10 pb-10 pt-20 lg:pb-20 lg:pt-[120px]">
-        <div className="container">
-          <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <SingleCard
-              image="https://i.ibb.co/r2zns1m/image-01.jpg"
-              CardTitle="50+ Best creative website themes & templates"
-              titleHref="/#"
-              btnHref="/#"
-              CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
-              Button="View Details"
-            />
-            <SingleCard
-              image="https://i.ibb.co/0nbbWM9/image-02-1.jpg"
-              CardTitle="Creative Card Component designs graphic elements"
-              titleHref="/#"
-              btnHref="/#"
-              CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
-              Button="View Details"
-            />
-            <SingleCard
-              image="https://i.ibb.co/dL9fH7N/image-03-1.jpg"
-              CardTitle="The ultimate UX and UI guide to card design"
-              titleHref="/#"
-              btnHref="/#"
-              CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
-              Button="View Details"
-            />
-            <SingleCard
-              image="https://i.ibb.co/dL9fH7N/image-03-1.jpg"
-              CardTitle="The ultimate UX and UI guide to card design"
-              titleHref="/#"
-              btnHref="/#"
-              CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
-              Button="View Details"
-            />
-            <SingleCard
-              image="https://i.ibb.co/dL9fH7N/image-03-1.jpg"
-              CardTitle="The ultimate UX and UI guide to card design"
-              titleHref="/#"
-              btnHref="/#"
-              CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
-              Button="View Details"
-            />
-            <SingleCard
-              image="https://i.ibb.co/dL9fH7N/image-03-1.jpg"
-              CardTitle="The ultimate UX and UI guide to card design"
-              titleHref="/#"
-              btnHref="/#"
-              CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
-              Button="View Details"
-            />
-          </div>
+    <section className="mt-10 pb-10 pt-20 lg:pb-20 lg:pt-[120px]">
+      <div className="container">
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <SingleCard
+                // key={post.id} // Ensure your backend provides a unique ID
+                image={post.image || "https://picsum.photos/id/237/200/300"}
+                CardTitle={post.title}
+                titleHref="/#"
+                btnHref="/#"
+                CardDescription={post.description}
+                Button="View Details"
+              />
+            ))
+          ) : (
+            <p>Loading posts...</p>
+          )}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
